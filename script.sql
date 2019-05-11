be0a5a35757404cbcc57abc7eb44a50ee70bd0d1
alexander_n <nlvk48@gmail.com>
2019-05-11 14:29:42 +0300

    sql script fix

--- script.sql
+++ script.sql
@@ -1,40 +1,96 @@
 USE fixapp;
+SET FOREIGN_KEY_CHECKS=0;
 
-DROP TABLE IF EXISTS users;
-
-CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL,
-    firstName VARCHAR(100),
-    lastName VARCHAR(100),
-    userType VARCHAR(20),
+-- ------------------------------------------
+-- fixapp.contacts
+-- ------------------------------------------
+DROP TABLE IF EXISTS contacts;
+CREATE TABLE IF NOT EXISTS contacts (
+    id INT AUTO_INCREMENT NOT NULL,
     email VARCHAR(100),
-    password VARCHAR(50),
+    phone VARCHAR(16),
     PRIMARY KEY (id)
-);
+    ) ENGINE=InnoDB CHARACTER SET=UTF8;
+INSERT INTO contacts (id, email, phone)
+VALUES (1, 'admin@fix.com', '+38067-777-77-77'),
+(2, 'manager@fix.com', '+38093-333-33-33'),
+(3, 'user@user.com', '+38098-475-21-30');
 
-INSERT INTO users (id, firstName, lastName, userType, email, password)
-VALUES (1, 'Admin', 'Admin', 'MANAGER', 'admin@gmail.com', 'admin'),
-(2, 'John', 'Smith', 'CUSTOMER', 'john-smith@gmail.com', 'pass'),
-(3, 'Master', 'Master', 'MASTER', 'master@gmail.com', 'master');
+-- ------------------------------------------
+-- fixapp.users
+-- ------------------------------------------
+DROP TABLE IF EXISTS users;
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT NOT NULL,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    role VARCHAR(20),
+    contacts_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (contacts_id) REFERENCES contacts (id)
+      ON DELETE CASCADE
+    ) ENGINE=InnoDB CHARACTER SET=UTF8;
+INSERT INTO users (id, username, password, first_name, last_name, role, contacts_id)
+VALUES (1, 'admin', 'admin', 'John','Smith', 'ADMIN', 1),
+(2, 'manager', 'manager', 'Jane','Richardson', 'MANAGER', 2),
+(3, 'user', 'user', 'Tony', 'Black', 'USER', 3);
 
-DROP TABLE IF EXISTS repairs;
+-- ------------------------------------------
+-- fixapp.items
+-- ------------------------------------------
+DROP TABLE IF EXISTS items;
+CREATE TABLE IF NOT EXISTS items (
+    id INT AUTO_INCREMENT NOT NULL,
+    product_type VARCHAR(100),
+    brand VARCHAR(100),
+    product_name VARCHAR(100),
+    PRIMARY KEY (id)
+    ) ENGINE=InnoDB CHARACTER SET=UTF8;
+INSERT INTO items (id, product_type, brand, product_name)
+VALUES (1, 'phone', 'Nokia', 'lumia 720'),
+(2, 'tv', 'Philips', 'HD200'),
+(3, 'car', 'ZAZ', 'Lanos'),
+(4, 'microwave', 'Samsung', 'HG100');
 
-CREATE TABLE IF NOT EXISTS repairs (
-    id BIGINT NOT NULL,
-    userId INT NOT NULL,
-    status VARCHAR(100),
-    item VARCHAR(100),
+-- ------------------------------------------
+-- fixapp.repair_requests
+-- ------------------------------------------
+DROP TABLE IF EXISTS repair_requests;
+CREATE TABLE IF NOT EXISTS repair_requests (
+    id INT AUTO_INCREMENT NOT NULL,
+    status VARCHAR(20),
+    user_id INT NOT NULL,
+    item_id INT NOT NULL,
     description VARCHAR(250),
-    pictureUrl VARCHAR(100),
-    price BIGINT,
-    managerNote VARCHAR(250),
-    userFeedback VARCHAR(250),
-    PRIMARY KEY (id)
-);
+    cost BIGINT DEFAULT NULL,
+      PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
+      ON DELETE CASCADE,
+    FOREIGN KEY (item_id) REFERENCES items (id)
+      ON DELETE CASCADE
+    ) ENGINE=InnoDB CHARACTER SET=UTF8;
+INSERT INTO repair_requests (id, status, user_id, item_id, description, cost)
+VALUES (1, 'NEW', 3, 1, 'not charging', null),
+(2, 'APPROVED', 3, 2, 'black stripes on the screen', 1000),
+(3, 'DECLINED', 3, 3, 'bad breaks', null),
+(4, 'DONE', 3, 4, 'spinning but not heating', 2890);
 
-INSERT INTO repairs (id, userId, status, item, description, pictureUrl, price, managerNote, userFeedback)
-VALUES (1, 2, 'NEW', 'Phone', 'not charging', NULL, NULL, NULL, NULL),
-(2, 2, 'APPROVED', 'Tv', 'black stripes on the screen', NULL, NULL, 'Need to change matrix', NULL),
-(3, 2, 'DECLINED', 'Car', 'bad breaks', NULL, NULL, 'Sorry, we don''t fix cars', NULL),
-(4, 2, 'DONE', 'Microwave', 'spinning but not heating', '', 1000,
-'Some problems with electronic components', 'Thank you for speed and quality.');
+-- ------------------------------------------
+-- fixapp.feedback
+-- ------------------------------------------
+DROP TABLE IF EXISTS feedback;
+CREATE TABLE IF NOT EXISTS feedback (
+    id INT AUTO_INCREMENT NOT NULL,
+    user_id INT NOT NULL,
+    text VARCHAR(255),
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
+      ON DELETE CASCADE
+    ) ENGINE=InnoDB CHARACTER SET=UTF8;
+INSERT INTO feedback (id, user_id, text)
+VALUES (1, 3, 'Thank you for quick repair.');
+
+
+SET FOREIGN_KEY_CHECKS=1;
